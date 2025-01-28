c6fb0c5c6bd30a6321c49f5b3b6f0dc0821c7f58
Oksana Shvanchenkova <oshvanchenkova@win.gabos.pl>
2025-01-28 16:25:02 +0100

    home work 7.2.2

--- type-assertions.ts
+++ type-assertions.ts
@@ -1,22 +1,32 @@
 function fetchData(): unknown {
-    return { name: "John Doe", age: 30 };
-  }
-  
-  type Person = {
-    name: string;
-    age: number;
-  };
-  
-  const data = fetchData(); 
-  
-  if (typeof data === "object" && data !== null && "name" in data && "age" in data) {
-    const person: Person = data as Person;
-  
-    function printPersonInfo(person: Person): void {
-      console.log(`Name: ${person.name}, Age: ${person.age}`);
-    }
-  
-    printPersonInfo(person);
-  } else {
-    console.error("Invalid data format");
-  }
+  return { name: "John Doe", age: 30 };
+}
+
+type Person = {
+  name: string;
+  age: number;
+};
+
+function isPerson(data: unknown): data is Person {
+  return (
+    typeof data === "object" &&
+    data !== null &&
+    "name" in data &&
+    "age" in data &&
+    typeof (data as any).name === "string" &&
+    typeof (data as any).age === "number"
+  );
+}
+
+function printPersonInfo(person: Person): void {
+  console.log(`Name: ${person.name}, Age: ${person.age}`);
+}
+
+const data = fetchData();
+
+if (isPerson(data)) {
+  const person: Person = data;
+  printPersonInfo(person);
+} else {
+  console.error("Invalid data format");
+}
